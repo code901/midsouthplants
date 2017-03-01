2d75c5d39b37780abdf39380cce3896012701866
Sean McKenna <sean.p.mckenna@icloud.com>
2017-03-01 14:50:39 -0600

    created initial site as a temporary landing page; created pm2 start config file

---
+++ app.js
@@ -0,0 +1,37 @@
+var port = process.env.PORT || 8080;
+
+var views_path = __dirname + '/src/views/';
+var views_engine = 'pug';
+var landing_view = 'home';
+// var css_path = __dirname + '/src/styles/css/';
+var css_request_base = '/css';
+var less_path = __dirname + '/src/styles/less/';
+
+
+var express = require('express');
+var expressLess = require('express-less');
+var compression = require('compression');
+
+var app = express();
+app.use(compression());
+
+app.use('/css', expressLess(less_path,
+  {
+    debug:    app.get('env') == 'development',
+    cache:    app.get('env') == 'production',
+    compress: app.get('env') == 'production'
+  }  
+));
+
+app.set('views', views_path);
+app.set('view engine', views_engine);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render(landing_view, {
+    title: 'MidSouth Plants'
+  });
+});
+
+app.listen(port);
